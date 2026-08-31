2468e144bdffece1b670e6b05bf595a4c0cafefe
James Daniel <jhdgamer05@gmail.com>
2026-08-31 18:54:18 -0400

    test: run trick residual acceptance in runtime gate

--- tests/runtime_quality_gate.ps1
+++ tests/runtime_quality_gate.ps1
@@ -44,6 +44,7 @@ $scenes = @(
 	"res://tests/crash_recovery_acceptance.tscn",
 	"res://tests/ski_feel_acceptance.tscn",
 	"res://tests/flick_trick_acceptance.tscn",
+	"res://tests/trick_residual_acceptance.tscn",
 	"res://tests/flick_gameplay_acceptance.tscn",
 	"res://tests/trick_ui_acceptance.tscn",
 	"res://tests/session_flow_acceptance.tscn",
